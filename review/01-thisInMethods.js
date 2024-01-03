bcae1ef251dd4934ae2d7449fe1864e0927b6bc0
mtx-dev <andrey.kozlitskiy@gmail.com>
2024-01-04 00:44:28 +0200

    update name

--- review/01-thisInMethods.js
+++ review/01-thisInMethods.js
@@ -30,23 +30,22 @@ const created = new Entity( () => this );
 created.outerFunc = outerFunc;
 created.outerArrow = outerArrow;
 
-console.log('\nObject methods');
+console.log('\n Object methods');
 console.log('func()   ', object.func() ? 'Yes' : 'undefined');
 console.log('arrow()  ', object.arrow() ? 'Yes' : 'undefined');
 
-object.func()
 
-console.log('\nMethods from constructor');
+console.log('\n Methods from constructor');
 console.log('incomingArrow() ', created.incomingArrow() ? 'Yes' : 'undefined');
 console.log('innerArrow()    ', created.innerArrow() ? 'Yes' : 'undefined');
 
 
-console.log('\nMethods from prototype');
+console.log('\n Methods from prototype');
 console.log('inProtoFunc()   ', created.inProtoFunc() ? 'Yes' : 'undefined');
 console.log('inProtoArrow()  ', created.inProtoArrow() ? 'Yes' : 'undefined');
 
 
-console.log('\nMethods assigned to created object');
+console.log('\n Methods assigned to created object');
 console.log('outerFunc()     ', created.outerFunc() ? 'Yes' : 'undefined');
 console.log('outerArrow()    ', created.outerArrow() ? 'Yes' : 'undefined');
 console.log();
